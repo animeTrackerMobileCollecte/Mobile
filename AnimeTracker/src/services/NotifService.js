dbf5a32e992a79f413fba3766446ee090e719de8
ChadiEC <1985081@collegemv.qc.ca>
2025-12-22 22:46:13 -0500

    Changement du notif system puisqu'il cassais l'app sur android studio

--- AnimeTracker/src/services/NotifService.js
+++ AnimeTracker/src/services/NotifService.js
@@ -1,48 +1,40 @@
 import * as Notifications from 'expo-notifications';
 import { Platform } from 'react-native';
 
-// Configuration du comportement (affichage même si l'app est ouverte)
+
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
     shouldPlaySound: true,
-    shouldSetBadge: true,
+    shouldSetBadge: false,
   }),
 });
 
-export const registerForNotifications = async () => {
-  const { status: existingStatus } = await Notifications.getPermissionsAsync();
-  let finalStatus = existingStatus;
-  
-  if (existingStatus !== 'granted') {
-    const { status } = await Notifications.requestPermissionsAsync();
-    finalStatus = status;
-  }
-  
-  if (finalStatus !== 'granted') {
-    console.log('Permission de notification refusée');
-    return false;
-  }
 
-  // Configuration spécifique pour Android (canaux)
+export const registerForNotifications = async () => {
+  const { status } = await Notifications.requestPermissionsAsync();
+  
   if (Platform.OS === 'android') {
-    Notifications.setNotificationChannelAsync('default', {
+    await Notifications.setNotificationChannelAsync('default', {
       name: 'default',
       importance: Notifications.AndroidImportance.MAX,
       vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
     });
   }
-  return true;
+  return status === 'granted';
 };
 
-export const sendNotification = (title, body) => {
-  Notifications.scheduleNotificationAsync({
-    content: {
-      title: title,
-      body: body,
-      sound: true,
-    },
-    trigger: null, // Envoi immédiat
-  });
+
+export const sendNotification = async (title, body) => {
+  try {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: title,
+        body: body,
+      },
+      trigger: null, 
+    });
+  } catch (error) {
+    console.log("Erreur notif:", error);
+  }
 };
